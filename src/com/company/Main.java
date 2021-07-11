8a5aca31de0660997d618700446387ebba15c30f
krishna <krishnasahoo99@gmail.com>
2021-07-11 21:55:45 +0530

    updated code in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hi");
+	//System.out.println("Hi");
+        int z =55+6;
+        System.out.println(z);
     }
 }
