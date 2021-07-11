109abc334fa283580660eab860fb799092ae02e4
krishnas-github <81104238+krishnas-github@users.noreply.github.com>
2021-07-12 00:19:54 +0530

    Update MyDev2.java

--- src/com/company/dev2/MyDev2.java
+++ src/com/company/dev2/MyDev2.java
@@ -4,7 +4,8 @@ public class MyDev2 {
     public void test(){
         Integer x =7+5;
         System.out.println("Test method");
-        Integer x = 5+6;
-        System.out.println(x);
+        //some changes
+        Integer x2 = 5+6;
+        System.out.println(x2);
     }
 }
