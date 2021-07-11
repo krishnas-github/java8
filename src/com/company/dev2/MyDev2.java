f086f9802bb4b1e2aa0b984245fb95d7e43a9c24
krishna <krishnasahoo99@gmail.com>
2021-07-11 21:44:48 +0530

    added addition logic

---
+++ src/com/company/dev2/MyDev2.java
@@ -0,0 +1,9 @@
+package com.company.dev2;
+
+public class MyDev2 {
+    public void test(){
+        System.out.println("test method");
+        Integer x =7+5;
+        System.out.println(x);
+    }
+}
