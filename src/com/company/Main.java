02678add28666fd544d2b5a00be33a07aa5e406e
krishna <krishnasahoo99@gmail.com>
2021-07-11 22:12:51 +0530

    updated the reviewer code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	//System.out.println("Hi");
-        int z =55+6;
+        Integer z =55+6;
         System.out.println(z);
     }
 }
